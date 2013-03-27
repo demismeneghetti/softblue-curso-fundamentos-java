9132188966d3c8e229620c936b42adfb1bbf97aa
Demis Meneghetti <demismeneghetti@msn.com>
2013-03-27 11:23:23 -0300

    innitial

---
+++ src/logicaDeProgramacao/escreva.java
@@ -0,0 +1,19 @@
+package logicaDeProgramacao;
+
+public class escreva {
+
+	int inteiro;
+	double real;
+	String caracter;
+	boolean logico;
+
+	{
+
+		inteiro = 100;
+		real = 2.50;
+		caracter = "texto";
+		logico = true;
+
+	}
+
+}
