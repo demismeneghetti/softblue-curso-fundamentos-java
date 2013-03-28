53fa38dd1550a19bedc66ea69caf7633e2000d3b
Demis Meneghetti <demismeneghetti@msn.com>
2013-03-28 18:37:39 -0300

    20130328-1837

---
+++ src/programacaoJava03ConceitosIniciais/aula342EstruturaDeControleSwitch.java
@@ -0,0 +1,27 @@
+package programacaoJava03ConceitosIniciais;
+
+public class aula342EstruturaDeControleSwitch {
+
+	public static void main(String[] args) {
+
+		int num = 20;
+
+		// Neste caso, trata-se de uma opção ao If/Else com algumas ressalvas!
+		switch (num) {
+		case 0:
+			System.out.println("Numero 0");
+			break;
+
+		case 10:
+			System.out.println("Numero 10");
+			break;
+
+		case 20:
+			System.out.println("Numero 20");
+			break;
+
+		default:
+			System.out.println("Outro número!");
+		}
+	}
+}
