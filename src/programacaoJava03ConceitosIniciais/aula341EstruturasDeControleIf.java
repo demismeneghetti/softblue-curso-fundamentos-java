237d81a8488fd98f60b4f6cb7bc31706815ef46c
Demis Meneghetti <demismeneghetti@msn.com>
2013-04-03 15:56:02 -0300

    20130403-1556

--- src/programacaoJava03ConceitosIniciais/aula341EstruturasDeControleIf.java
+++ src/programacaoJava03ConceitosIniciais/aula341EstruturasDeControleIf.java
@@ -4,21 +4,22 @@ public class aula341EstruturasDeControleIf {
 
 	public static void main(String[] args) {
 
-		// Ao alterar o valor da variavel e executando a classe 
-		// o resultado e exibido de acordo com o informado no codigo.
-		int num = 5;
+		/* Ao alterar o valor da variavel e executando a classe 
+		o resultado e exibido de acordo com o informado no codigo. */
+		
+		int num = 10;
 
 		// Se o valor for maior que 10
 		if (num > 10) {
-			System.out.println("Numero maior do que 10");
+			System.out.println("Numero MAIOR do que 10");
 		
 		// Se o valor for menor que 10
 		} else if (num < 10){
-			System.out.println("Numero menor do que 10");
+			System.out.println("Numero MENOR do que 10");
 		
 		// Se o valor for igual a 10
 		} else {
-			System.out.println("Numero igual a 10");
+			System.out.println("Numero IGUAL a 10");
 		}
 	}
 }
