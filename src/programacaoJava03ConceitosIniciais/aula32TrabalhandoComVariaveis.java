4d483beff979cc535f0e7adaa013b488eef3cac1
Demis Meneghetti <demismeneghetti@msn.com>
2013-04-04 15:39:45 -0300

    20130404-1539

--- src/programacaoJava03ConceitosIniciais/aula32TrabalhandoComVariaveis.java
+++ src/programacaoJava03ConceitosIniciais/aula32TrabalhandoComVariaveis.java
@@ -5,7 +5,6 @@ public class aula32TrabalhandoComVariaveis {
 	public static void main(String[] args) {
 
 		// Este metodo, ao ser executado, exibira o resultado da variavel X
-		// Este metodo, ao ser executado, exibira o resultado da variavel X
 		int x;
 		x = 500;
 		System.out.println(x);
