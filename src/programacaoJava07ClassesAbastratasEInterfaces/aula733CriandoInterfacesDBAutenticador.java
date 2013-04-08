aaad33b9b5aabb61c2b3da7a2b020dc3d90b1d8e
Demis Meneghetti <demismeneghetti@msn.com>
2013-04-07 23:17:07 -0300

    20130407-2317

---
+++ src/programacaoJava07ClassesAbastratasEInterfaces/aula733CriandoInterfacesDBAutenticador.java
@@ -0,0 +1,10 @@
+package programacaoJava07ClassesAbastratasEInterfaces;
+
+public class aula733CriandoInterfacesDBAutenticador implements aula732CriandoInterfacesAutenticador{
+
+	public boolean autenticar (String usuario, String senha) {
+		
+		//Checa se o usuario e valido.
+		return true;
+	}
+}
---
+++ src/programacaoJava07ClassesAbastratasEInterfaces/aula734CriandoInterfacesDBFileAutenticar.java
@@ -0,0 +1,11 @@
+package programacaoJava07ClassesAbastratasEInterfaces;
+
+public class aula734CriandoInterfacesDBFileAutenticar implements aula732CriandoInterfacesAutenticador {
+
+	public boolean autenticar(String usuario, String senha){
+		
+		//Autenticacao baseada em arquivo
+		
+		return true;
+	}
+}
